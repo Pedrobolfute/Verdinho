fd46d70c8e663045b5de599f76c8e6e04cc0bea2
pedrobolfute <pepenike-101@hotmail.com>
2023-01-01 00:00:00 -0300

    1 JAN

--- commits.sh
+++ commits.sh
@@ -1,60 +1,61 @@
 #!/bin/bash
 
 func-commit(){
-caminhoDir="/home/pedro/workspace/Verdinho"
-meses=(JAN FEB MAR APR MAY JUN JUL AUG SEP OCT NOV DEC)
+  ##Aqui.
+  caminhoDir="/home/pedro/workspace/Verdinho"
+  ##Aqui.
+  meuUser="pedro"
+  meses=(Empty JAN FEB MAR APR MAY JUN JUL AUG SEP OCT NOV DEC)
 
-for i in $(seq 1 12);
-do 
-  if [ "${meses[$i]}" = "JAN" ] || [ "${meses[$i]}" = "MAR" ] || [ "${meses[$i]}" = "MAY" ] || [ "${meses[$i]}" = "JUL" ] || [ "${meses[$i]}" = "AUG" ] || [ "${meses[$i]}" = "OCT" ] || [ "${meses[$i]}" = "DEC" ]
-  then
-    for j in $(seq 1 31)
-    do
-    sudo date -s "${meses[$i]} $j"
-    
-    mkdir -p $caminhoDir/evento1/evento-$j-$i
-    echo "PedrobolfuteFix $j / $i / 2023" >> $caminhoDir/evento1/evento-$j-$i/arquivo$j
+  for i in $(seq 1 12);
+  do 
+    if [ "${meses[$i]}" = "JAN" ] || [ "${meses[$i]}" = "MAR" ] || [ "${meses[$i]}" = "MAY" ] || [ "${meses[$i]}" = "JUL" ] || [ "${meses[$i]}" = "AUG" ] || [ "${meses[$i]}" = "OCT" ] || [ "${meses[$i]}" = "DEC" ]
+    then
+      for j in $(seq 1 31)
+      do
+      sudo date -s "${meses[$i]} $j"
 
-    cd $caminhoDir
-    git add .
-    git commit -m "$j ${meses[$i]}"
-    ##git push origin main
-    done
-  elif [ "${meses[$i]}" = "APR" ] || [ "${meses[$i]}" = "JUN" ] || [ "${meses[$i]}" = "SEP" ] || [ "${meses[$i]}" = "NOV" ]
-  then
-    for j in $(seq 1 30)
-    do
-    sudo date -s "${meses[$i]} $j"
-    
-    mkdir -p $caminhoDir/evento1/evento-$j-$i
-    echo "PedrobolfuteFix $j / $i / 2023" >> $caminhoDir/evento1/evento-$j-$i/arquivo$j
-    
-    cd $caminhoDir 
-    git add .
-    git commit -m "$j ${meses[$i]}"
-    ##git push origin main
-    done
-  elif [ "${meses[$i]}" = "FEB" ]
-  then
-    for j in $(seq 1 28)
-    do
-    sudo date -s "${meses[$i]} $j"
-    
-    mkdir -p $caminhoDir/evento1/evento-$j-$i
-    echo "PedrobolfuteFix $j / $i / 2023" >> $caminhoDir/evento1/evento-$j-$i/arquivo$j
-    
-    cd $caminhoDir 
-    git add .
-    git commit -m "$j ${meses[$i]}"
-    ##git push origin main
-    done
-  else
-  echo "Error dentro do IF!"
-  fi
-done
+      mkdir -p $caminhoDir/evento1/evento-$j-$i
+      echo "PedrobolfuteFix $j / $i / 2023" >> $caminhoDir/evento1/evento-$j-$i/arquivo$j
 
-date -s "feb 19 15:10"
+      cd $caminhoDir
+      git add .
+      git commit -m "$j ${meses[$i]}"
+      done
+    elif [ "${meses[$i]}" = "APR" ] || [ "${meses[$i]}" = "JUN" ] || [ "${meses[$i]}" = "SEP" ] || [ "${meses[$i]}" = "NOV" ]
+    then
+      for j in $(seq 1 30)
+      do
+      sudo date -s "${meses[$i]} $j"
 
-chown -R pedro:pedro $caminhoDir/*
+      mkdir -p $caminhoDir/evento1/evento-$j-$i
+      echo "PedrobolfuteFix $j / $i / 2023" >> $caminhoDir/evento1/evento-$j-$i/arquivo$j
+
+      cd $caminhoDir 
+      git add .
+      git commit -m "$j ${meses[$i]}"
+      done
+    elif [ "${meses[$i]}" = "FEB" ]
+    then
+      for j in $(seq 1 28)
+      do
+      sudo date -s "${meses[$i]} $j"
+
+      mkdir -p $caminhoDir/evento1/evento-$j-$i
+      echo "PedrobolfuteFix $j / $i / 2023" >> $caminhoDir/evento1/evento-$j-$i/arquivo$j
+
+      cd $caminhoDir 
+      git add .
+      git commit -m "$j ${meses[$i]}"
+      done
+    else
+    echo "Error dentro do IF!"
+    fi
+  done
+
+  ##Aqui.
+  date -s "feb 19 15:40"
+  chown -R pedro:pedro $caminhoDir/*
 }
-func-commit >> log
+
+func-commit >> logCommits
